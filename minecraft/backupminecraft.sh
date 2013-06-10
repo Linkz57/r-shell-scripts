1e2a10aca2ea0dbcc571d06e995edbeaa776d4bb
Tyler Francis <linkz57@gmail.com>
2013-06-09 20:32:19 -0400

    Now backs up to remote machine via rsync. Changed date to comply with ISO 8601

--- minecraft/backupminecraft.sh
+++ minecraft/backupminecraft.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/[username]/minecraft
-cp -R -v server server\ backup/server.bak_$(date +%m-%d-%y_%I-%M%P) && ls -l --color server\ backup/server.bak_$(date +%m-%d-%y_%I-%M%P)
-ls -l --color server\ backup
+rsync -av server [username]@[ServerIP]::[folder]/minecraftbackups/server.bak_$(date +%Y-%m-%d_%H-%M)
 echo -e "\n\n           ~ ~ B a c k u p   C o m p l e t e ! ~ ~ \n\n"
