396a577b00161f83429f134fb6208db884d4bd98
Tyler Francis <Linkz57@gmail.com>
2014-02-24 15:08:46 -0600

    Now warns when about to shut down
    Also asks for root earlier so I can walk away 4 seconds sooner.

--- automation/end.sh
+++ automation/end.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# end.sh v1.2
 wmctrl -c rhythmbox
+sudo ls > /dev/null
 sleep 2
 killall rhythmbox
 sleep 2
 sudo umount /media/69ee332c-0039-4c2d-93e9-8e397d4857b2/
 sudo cryptsetup luksClose mahdrive
+echo "done with all of that. Shutting down, now"
 sleep 10
 sudo shutdown -h now
