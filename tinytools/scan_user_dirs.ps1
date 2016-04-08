75ac8f9861017eb0675170b2e1ae7b3c1f67879a
Tyler Francis <Linkz57@gmail.com>
2016-04-08 09:03:35 -0500

    PowerShell script for measuring all home folders
    Been thinking about migrating data, and wanted to get some info from a bunch of machines. I put the names in a text file, one per line, and fed it into this. Line 4 (where most of the work happens) I stole from someone else, and now can't remember. I'm a jerk.
    
    For some reason, sometimes the Tee-Object outputs to screen and not to file, so I added line 6 as a backup.

--- tinytools/scan_user_dirs.ps1
+++ tinytools/scan_user_dirs.ps1
@@ -3,5 +3,5 @@ for ($i=0; $i -lt $computerNameArray.length; $i++) {
 	$scanFolder = "\\" + $computerNameArray[$i] + "\c$\users"
 	$scanResult = $computerNameArray[$i] + " weighs {0:0,0.00} GB" -f ((Get-ChildItem -R $scanFolder | measure-object length -Sum).Sum / 1GB)
 	echo $scanResult | Tee-Object -file user_dir_sizes.txt
-  scanResult >> user_dir_sizes-backup.txt
+	scanResult >> user_dir_sizes-backup.txt
 }
