a4dcc57f83d51d28c1d7938ac4d908c72ed3445e
Tyler Francis <Linkz57@gmail.com>
2019-01-04 10:02:05 -0600

    Tiny update to fix head command

--- automation/vmTemplateFirstRun.sh
+++ automation/vmTemplateFirstRun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## vmTemplateFirstRun.sh
-## Version 1.0
+## Version 1.1
 ##
 ## I want to run this script to safely make a clone of a machine
 ## and I want this clone to live safely on the same network with many of its other clones, concurrently
@@ -60,7 +60,7 @@ if hostname | grep -P '(^|\s)\Ktemplatemachn(?=\s|$)' > /dev/null ; then
 	## set up static IP
 	echo "Changing IP address..."
 	sudo cp /etc/network/interfaces /etc/network/interfaces.$(\date +%Y-%m-%d_%H-%M).bak
-	firstBitOfInterfaces=$(head /etc/network/interfaces -n $(grep -n "inet static" /etc/network/interfaces | awk -F ':' '{ print $1 }'))
+	firstBitOfInterfaces=$(head /etc/network/interfaces -n$(grep -n "inet static" /etc/network/interfaces | awk -F ':' '{ print $1 }'))
 	echo "$firstBitOfInterfaces" | sudo tee /etc/network/interfaces
 	echo "address $iaddress" | sudo tee -a /etc/network/interfaces
 	echo "netmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
